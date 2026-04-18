2bcf7e3cd78c518347ad81c6f4e92f3e5187afa8
Robo-Alex14 <alexandramdlv14@gmail.com>
2026-04-18 14:00:52 -0400

    Update Node.cpp

--- src/Node.cpp
+++ src/Node.cpp
@@ -1,3 +1,8 @@
 #include "Node.h"
 
-//a vous de jouer
+using namespace std;
+
+namespace dna{
+    Node::Node(const uint64_t& p_kmer, const GenomicPosition& p_pos):
+        m_kmerCode(p_kmer), m_position(p_pos), m_next(nullptr) {};
+}
