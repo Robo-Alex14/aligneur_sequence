71a35232f0a6b10fae1b2c1bb96a97e0f8cf251d
Robo-Alex14 <alexandramdlv14@gmail.com>
2026-04-18 14:00:21 -0400

    Update LinkedList.cpp

--- src/LinkedList.cpp
+++ src/LinkedList.cpp
@@ -1,3 +1,27 @@
 #include "LinkedList.h"
 
-//a vous de jouer
+namespace dna{
+    
+    LinkedList::LinkedList() : m_head(nullptr){};
+    
+    //Elimination de tout les noeuds contenus dans la linked list
+    LinkedList::~LinkedList() {
+        Node* current = m_head;
+        while(current != nullptr){
+            Node* temp = current;
+            current = current->m_next;
+            delete temp;
+        }
+    }
+    
+    void LinkedList::insert(const uint64_t& p_kmerCode, const GenomicPosition& p_pos){
+        Node* insertion = new Node(p_kmerCode, p_pos);
+        insertion->m_next = m_head;
+        m_head = insertion;
+    };
+    
+    Node* LinkedList::first() const{
+        return m_head;
+    };
+    
+}
