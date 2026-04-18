39d956172927f22e4ccb7104d686b146acbc9604
Robo-Alex14 <alexandramdlv14@gmail.com>
2026-04-18 13:58:23 -0400

    Update AlignmentResult.cpp

--- src/AlignmentResult.cpp
+++ src/AlignmentResult.cpp
@@ -1,14 +1,19 @@
 #include "AlignmentResult.h"
 
-AlignmentResult::AlignmentResult()
-    : position(), mismatches(0), matches(0) {}
+namespace dna {
 
-AlignmentResult::AlignmentResult(const GenomicPosition& pos, int mm, int m)
-    : position(pos), mismatches(mm), matches(m) {}
+AlignmentResult::AlignmentResult()
+    : m_position(), m_mismatches(0), m_matches(0) {};
+
+AlignmentResult::AlignmentResult(const GenomicPosition& p_pos, int p_mismatches, 
+        int p_matches)
+    : m_position(p_pos), m_mismatches(p_mismatches), m_matches(p_matches) {};
 
 bool AlignmentResult::operator<(const AlignmentResult& o) const {
-    return mismatches < o.mismatches;
-}
+    return m_mismatches < o.m_mismatches;
+};
+
 bool AlignmentResult::operator>(const AlignmentResult& o) const {
-    return mismatches > o.mismatches;
+    return m_mismatches > o.m_mismatches;
+};
 }
